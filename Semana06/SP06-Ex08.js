7b4d2dd3f21aa3263fb52aa84790c67207578ee4
LucasFL <lucas.f.lotaif@gmail.com>
2021-10-06 11:14:37 -0300

    SP06-Ex08

---
+++ Semana06/SP06-Ex08.js
@@ -0,0 +1,9 @@
+import {dividirPorDois, aoQuadrado} from './funcoes.js';
+
+document
+    .getElementById('dividirPorDois')
+    .addEventListener('click', dividirPorDois);
+
+document
+    .getElementById('aoQuadrado')
+    .addEventListener('click', aoQuadrado);
