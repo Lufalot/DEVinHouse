61e1cc2422be2b3b7c0ec02bd1609e263cd4319b
LucasFL <lucas.f.lotaif@gmail.com>
2021-10-06 17:28:42 -0300

    SP06-Ex10

--- Semana06/cronometro/principal.js
+++ Semana06/cronometro/principal.js
@@ -1,7 +1,11 @@
 import Temporizador from './cronometro.js';
+import Dados from './Dados.js';
 
 let temporizador = new Temporizador();
-  
+let dados = new Dados();
+
+dados.carregar();
+
 var btnStart = document.querySelector('#button-start');
 var btnStop = document.querySelector('#button-stop');
 var btnReset = document.querySelector('#button-reset');
@@ -12,8 +16,10 @@ btnStart.addEventListener("click", function () {
   
 btnStop.addEventListener("click", function () {
     temporizador.parar();
+    dados.salvar();
 });
   
 btnReset.addEventListener("click", function () {
     temporizador.reset();
+    dados.limpar();
 });
