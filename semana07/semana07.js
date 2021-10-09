26437e7bdc8db3a62c4d18a52b4ccea0ee86fc67
LucasFL <lucas.f.lotaif@gmail.com>
2021-10-09 14:14:51 -0300

    Ex6

--- semana07/semana07.js
+++ semana07/semana07.js
@@ -1,16 +1,6 @@
 const button = document.querySelector('input');
 
-function joke() {
-        fetch('https://api.chucknorris.io/jokes/random')
-            .then(response => response.json())
-            .then(data => {
-                let { created_at, icon_url, id, url, value } = data;
-                new Piada(created_at, icon_url, id, url, value);
-            })
-            
-};
-
-button.addEventListener('click', joke);
+let jokeData = {};
 
 class Piada {
     constructor(dataCriacao, urlIcone, id, url, piada) {
@@ -20,4 +10,32 @@ class Piada {
         this.url = url,
         this.piada = piada
     }
+
+    criarElemento() {
+        let div = document.createElement('div');
+        let img = document.createElement('img');
+        let span = document.createElement('span');
+        
+        img.src = this.urlIcone;
+        span.textContent = `${this.piada} ${this.dataCriacao}`;
+
+        div.appendChild(img);
+        div.appendChild(span);
+        document.body.appendChild(div);
+    }
 }
+
+function joke() {
+        fetch('https://api.chucknorris.io/jokes/random')
+            .then(response => response.json())
+            .then(data => {
+                let { created_at, icon_url, id, url, value } = data;
+                jokeData = new Piada(created_at, icon_url, id, url, value);
+                console.log(jokeData);            
+                jokeData.criarElemento();
+            })
+            
+};
+
+button.addEventListener('click', joke);
+
