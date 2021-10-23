4b6f872cabaf6f8fb91a41ead6508c92f2ac8083
LucasFL <lucas.f.lotaif@gmail.com>
2021-10-23 15:02:59 -0300

    SP08-Ex04 fix

--- semana08/exercicios/src/components/Button/button.js
+++ semana08/exercicios/src/components/Button/button.js
@@ -1,18 +1,12 @@
 import React from "react";
 
 class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.title = this.props.title;
-    this.bgcolor = this.props.bgcolor;
-  }
-
   render() {
     return (
       <input
         type="button"
-        value={this.title}
-        style={{ backgroundColor: this.bgcolor, color: "white" }}
+        value={this.props.title}
+        style={{ backgroundColor: this.props.bgcolor, color: "white" }}
       />
     );
   }
