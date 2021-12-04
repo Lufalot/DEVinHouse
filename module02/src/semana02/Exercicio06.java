4ca400800a71c7c2794632510e66ba4847f0948f
LucasFL <lucas.f.lotaif@gmail.com>
2021-12-04 13:27:43 -0300

    M2S02-Ex6

--- module02/src/semana02/Exercicio06.java
+++ module02/src/semana02/Exercicio06.java
@@ -17,10 +17,10 @@ public class Exercicio06 {
         System.out.println("Digite o preço da gosalina: ");
         precoGasolina = keyboard.nextFloat();
 
-        System.out.println("Digite o rendimento do carro com etanol: ");
+        System.out.println("Digite o rendimento do carro com etanol (Km/L): ");
         rendimentoEtanol = keyboard.nextInt();
 
-        System.out.println("Digite o rendimento do carro com gasolina: ");
+        System.out.println("Digite o rendimento do carro com gasolina (Km/L): ");
         rendimentoGasolina = keyboard.nextInt();
 
         System.out.println("Digite a kilometragem rodada: ");
@@ -31,6 +31,12 @@ public class Exercicio06 {
 
         System.out.println("O custo da viajem com etanol é " + custoEtanol);
         System.out.println("O custo da viajem com gasolina é " + custoGasolina);
+
+        if (custoEtanol < custoGasolina) {
+            System.out.println("Combustível recomendado: Etanol.");
+        } else {
+            System.out.println("Combustível recomendado: Gasolina.");
+        }
     }
 
 }
